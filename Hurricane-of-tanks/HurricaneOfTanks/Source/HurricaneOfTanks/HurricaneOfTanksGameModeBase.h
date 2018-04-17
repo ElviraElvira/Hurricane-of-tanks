87b238c3bc7e6b42852864e21e8d7ac33d56957e
DESKTOP-BMN73DE\Ελβιρα <wizard1050@outlook.com>
2018-04-17 19:54:10 +0200

    Tanks00

---
+++ Hurricane-of-tanks/HurricaneOfTanks/Source/HurricaneOfTanks/HurricaneOfTanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HurricaneOfTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HURRICANEOFTANKS_API AHurricaneOfTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
